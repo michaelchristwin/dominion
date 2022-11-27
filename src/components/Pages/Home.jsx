f36cc2bdd6765bfbc5e3c06ab86e9c8a0452e948
michaelchristwin <kelechukwuchristwin@gmail.com>
2022-11-27 08:44:13 +0100

    Created portfolio section

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,6 +1,8 @@
 import "../CSS/styles.css";
 import Logo from "../JSX/Logo";
 import Sidebar from "../JSX/Sidebar";
+import Switchlg from "../img/logos/switchlg.png";
+import LaunchIcon from "@mui/icons-material/Launch";
 
 function Home() {
   const nums = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -26,6 +28,23 @@ function Home() {
             <h4 className="text-center text-white">TOOLKIT</h4>
           </div>
           <aside className="aside2 d-flex py-3">{logoElement}</aside>
+          <div className="bg-black rounded">
+            <h4 className="text-center">PORTFOLIO</h4>
+          </div>
+          <aside className="aside3 text-center">
+            <img src={Switchlg} alt="Switch logo" style={{ height: "40px" }} />1
+            year internship at Switch Electric Ltd as a Junior Front End Dev
+            <span>
+              <a href="https://whynotswitch.github.io/Switch-NFT-Marketplace-React/">
+                <LaunchIcon />
+              </a>
+            </span>
+            <div>
+              <span>
+                <p className="text-decoration-underline">Responsibilities</p>
+              </span>
+            </div>
+          </aside>
         </section>
       </div>
     </main>
